40b8ddfad725d07b973918993d7f11d82cb60e19
gdstorage <gadisanandita@gmail.com>
2024-08-02 14:41:17 +0700

    first

---
+++ app/src/main/java/id/ac/astra/polman/soal/A/pmb_chat_bot/MainActivity.java
@@ -0,0 +1,14 @@
+package id.ac.astra.polman.soal.A.pmb_chat_bot;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
